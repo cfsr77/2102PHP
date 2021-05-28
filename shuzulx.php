78f5acfe8ed4b30f14a9ab63dd78be7c858c5ae2
unknown <123456789@qq.com>
2021-05-28 11:22:39 +0800

    随机字符串

--- shuzulx.php
+++ shuzulx.php
@@ -7,4 +7,17 @@
     }else {
         echo "$bb 不存在";
     }
+    echo "<hr>";
     
+
+    $arr1 = [123,214,213,56,33,11,789,'a','hello',"php"];
+    $arr2 = [
+        "name"  => "zhangsan",
+        "age"   =>  22,
+    ];
+    //把$arr1数组的个数 放到$length 里面
+    $length = count($arr1);
+    //通过for循环循环他的个数
+    for($i=0;$i<$length;$i++){
+        echo 'i=' . $i , '>>>' . $arr1[$i]. '<br>';
+    }
